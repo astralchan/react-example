104832692548d78aff41e8d99b9b39bedb3d54e9
astralchan <astral@astralchan.xyz>
2024-04-21 18:08:46 -0600

    custom theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,20 @@ export default {
   },
   plugins: [daisyui],
   daisyui: {
-    themes: ['dark']
+    themes: [
+      {
+        pixeltheme: {
+          "primary": "#9200ff",
+          "secondary": "#0600ff",
+          "accent": "#00ffbf",
+          "neutral": "#070b14",
+          "base-100": "#111827",
+          "info": "#0094e9",
+          "success": "#00ae00",
+          "warning": "#ffaf00",
+          "error": "#de2d49"
+        }
+      }
+    ]
   }
 };
